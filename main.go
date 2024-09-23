032641a0aaf91e031206d8e6af23dd053a88be2e
ssk <1sqlroot@gmail.com>
2024-09-23 18:35:25 +0400

    some stuff

--- main.go
+++ main.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"fmt"
+	"html/template"
+	"net/http"
 )
 
 type Rsvp struct {
@@ -10,17 +12,37 @@ type Rsvp struct {
 }
 
 var responses = make([]*Rsvp, 0, 10)
-var template = make(map[string]*template.Template, 3)
+
+var templates = make(map[string]*template.Template, 3)
 
 func loadTemplates() {
 
-	//TODO - load templates here
+	templateNames := [5]string{"welcome", "form", "thanks", "sorry", "list"}
+	for index, name := range templateNames {
 
+		t, err := template.ParseFiles("layout.html", name+".html")
+		if err == nil {
+			templates[name] = t
+			fmt.Println("Loaded template", index, name)
+
+		} else {
+			panic(err)
+		}
+	}
+}
+
+func welcomeHandler(writer http.ResponseWriter, request *http.Request) {
+	templates["welcome"].Execute(writer, responses)
+}
+
+func listHandler(writer http.ResponseWriter, request *http.Request) {
+	templates["list"].Execute(writer, nil)
 }
 
 func main() {
 
 	loadTemplates()
-	fmt.Println("s")
+	http.HandleFunc("/", welcomeHandler)
+	http.HandleFunc("/list", listHandler)
 
 }
